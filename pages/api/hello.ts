9071a1d32380a2de0329eeaaa054f2aca2c55f5a
Sebastian Altamirano <ridnois@protonmail.com>
2021-12-04 13:41:34 -0300

    initial implementation

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,10 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import mqtt from 'mqtt';
 
 type Data = {
   name: string
 }
-
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
